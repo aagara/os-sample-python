653fde9ea903f887b3460753d749c15fda44bee1
aagara <anarunkumar@yahoo.com>
2024-10-09 13:22:25 -0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from striprtf.striprtf import rtf_to_text
+
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
+    rtf = "some rtf encoded string"
+    text = rtf_to_text(rtf)
+    print(text)
     return "strip rtf package test!"
 
 if __name__ == "__main__":
